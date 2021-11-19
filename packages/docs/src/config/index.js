dd1165ee81ca6f258b19ae7bea39635fad30538c
ZhengYuZi <381740310@qq.com>
2021-11-19 16:33:22 +0800

    update docs

---
+++ packages/docs/src/config/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "TIRO UI",
+  themeConfig: {
+    //   头部导航
+    nav: [
+      {
+        text: "首页",
+        link: "/", //   侧边导航
+        sidebar: [{ text: "我的", link: "/mine/" }],
+      },
+      { text: "关于", link: "/about/" },
+    ],
+  },
+}
