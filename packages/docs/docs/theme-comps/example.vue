2feff888bfb34ebbf1994969830b9501b0fb9c6d
ZhengYuZi <381740310@qq.com>
2021-12-31 16:15:03 +0800

    update example-code style

--- packages/docs/docs/theme-comps/example.vue
+++ packages/docs/docs/theme-comps/example.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="ti-example">
-        <div class="example-show">
-            <slot name="component"></slot>
-        </div>
-        <div @click="codeShow">点击</div>
-        <div class="example-code" v-show="isShow">
-            <slot></slot>
-        </div>
+        <section class="example-box">
+            <div class="example-show">
+                <slot name="component"></slot>
+            </div>
+            <div class="example-handle">
+                <ti-icon name="icon-eye" @click="codeShow" />
+            </div>
+            <div class="example-code" v-show="isShow">
+                <slot></slot>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -21,5 +25,25 @@ const codeShow = () => {
 <style scoped>
 .ti-example {
     margin: 20px 0px;
+}
+.example-box {
+    border: 1px solid #f0f0f0;
+    border-radius: 2px;
+}
+.example-show {
+    padding: 30px;
+}
+.example-handle {
+    width: 100%;
+    height: 30px;
+    border-top: 1px dashed #f0f0f0;
+    text-align: center;
+    color: #cccccc;
+}
+.example-handle > * {
+    cursor: pointer;
+}
+.example-code {
+    border-top: 1px dashed #f0f0f0;
 }
 </style>
